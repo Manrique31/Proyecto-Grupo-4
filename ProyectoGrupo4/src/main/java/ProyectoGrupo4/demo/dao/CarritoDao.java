24477157a1a9da98dfd1f5de9b13bc744f717a6b
Manrique31 <144976323+Manrique31@users.noreply.github.com>
2024-11-25 20:21:10 -0600

    mas cambios
    sigue sin funcionar el carrito

--- ProyectoGrupo4/src/main/java/ProyectoGrupo4/demo/dao/CarritoDao.java
+++ ProyectoGrupo4/src/main/java/ProyectoGrupo4/demo/dao/CarritoDao.java
@@ -3,6 +3,7 @@ package ProyectoGrupo4.demo.dao;
 import java.util.List;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
