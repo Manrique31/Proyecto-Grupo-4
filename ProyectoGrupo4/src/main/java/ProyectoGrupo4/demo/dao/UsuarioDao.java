56d57badc13633e6c2c302932cb6eb6cee50c23f
Sebas090909 <tanyr09@gmail.com>
2024-12-08 00:19:19 -0600

    actualizado

---
+++ ProyectoGrupo4/src/main/java/ProyectoGrupo4/demo/dao/UsuarioDao.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package ProyectoGrupo4.demo.dao;
+
+import ProyectoGrupo4.demo.domain.Usuario;
+
+
+
+public interface UsuarioDao  {
+    Usuario findByUsername(String username);
+    
+    Usuario findByUsernameAndPassword(String username, String Password);
+
+    Usuario findByUsernameOrCorreo(String username, String correo);
+
+    boolean existsByUsernameOrCorreo(String username, String correo);
+}
+
